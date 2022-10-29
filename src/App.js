b33f729b20a9e937b3347c59422325a0cb9107bf
nonamenonlike <116864221+nonamenonlike@users.noreply.github.com>
2022-10-29 15:21:24 +0700

    initial project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"
+      meta charset="UTF-8"
+      http-equiv="X-UA-Compatible" content="IE=edge,width=device-width, initial-scale=1.0"
+      name="viewport"
+      >
+        <a
+          className="App-Login-Line"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossorigin="anonymous"
+          target="_blank"
+          rel="stylesheet"
+        a/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
