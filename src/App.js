9277f7f21b8105c4c06c78fc33fb7b5ee4eaed69
nonamenonlike <116864221+nonamenonlike@users.noreply.github.com>
2022-10-29 15:59:17 +0700

    add liff

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
+import logo from './logo.svg';
 import './App.css';
+import liff from '@line/liff';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [pictureUrl, setPictureUrl] = useState(logo);
+  const [idToken, setIdToken] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [statusMessage, setStatusMessage] = useState("");
+  const [userId, setUserId] = useState("");
+
+  const logout = () => {
+    liff.logout();
+    window.location.reload();
+  }
+
+  const initLine = () => {
+    liff.init({ liffId: '1657602972-2YVwAKWz' }, () => {
+      if (liff.isLoggedIn()) {
+        runApp();
+      } else {
+        liff.login();
+      }
+    }, err => console.error(err));
+  }
+
+  const runApp = () => {
+    const idToken = liff.getIDToken();
+    setIdToken(idToken);
+    liff.getProfile().then(profile => {
+      console.log(profile);
+      setDisplayName(profile.displayName);
+      setPictureUrl(profile.pictureUrl);
+      setStatusMessage(profile.statusMessage);
+      setUserId(profile.userId);
+    }).catch(err => console.error(err));
+  }
+
+  useEffect(() => {
+    initLine();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header"
-      meta charset="UTF-8"
-      http-equiv="X-UA-Compatible" content="IE=edge,width=device-width, initial-scale=1.0"
-      name="viewport"
-      >
-        <a
-          className="App-Login-Line"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossorigin="anonymous"
-          target="_blank"
-          rel="stylesheet"
-        a/>
+      <header className="App-header">
+      <div style={{ textAlign: "center" }}>
+        <h1>React With LINE Login To Website Free Picture Ragnarok</h1>
+        <hr/>
+        <img src={pictureUrl} width="300px" height="300px"/>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>id token: </b> {idToken}</p>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>display name: </b> {displayName}</p>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>status message: </b> {statusMessage}</p>
+        <p style={{ textAlign: "left", marginLeft: "20%", marginRight: "20%", wordBreak: "break-all" }}><b>user id: </b> {userId}</p>
+
+        <button onClick={() => logout()} style={{ width: "100%", height: 30 }}>Logout</button>
+      </div>
       </header>
     </div>
   );
